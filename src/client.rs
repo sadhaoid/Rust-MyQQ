3eeee1951946f1cb0f33bcd906c917adc6918c2d
舒俊鹏 <sjp@yuxiaor.com>
2025-10-23 14:41:45 +0800

    used stdout

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::io;
+use std::process::Output;
 use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader, stdin, stdout};
 use tokio::net::TcpStream;
 use tokio::time::{Duration, sleep};
@@ -8,7 +9,7 @@ use tokio::{self, stream};
 use crate::handle_client::{change_function, login_function, send_function};
 
 pub async fn start_client() -> Result<(), Box<dyn Error>> {
-    let mut stream: TcpStream = TcpStream::connect("127.0.0.1:8080").await?;
+    let mut stream: TcpStream = TcpStream::connect("192.168.60.66:8080").await?;
 
     let (mut reader, mut writer) = stream.into_split();
 
@@ -32,17 +33,24 @@ pub async fn start_client() -> Result<(), Box<dyn Error>> {
     //读服务端(读stream)，显示在控制台
     let y = tokio::spawn(async move {
         let mut buf_reader = BufReader::new(reader);
+        let mut client_output = stdout();
         loop {
             //println!("loop 2 is running");
             let mut input = String::new();
             match buf_reader.read_line(&mut input).await {
                 Ok(0) => {
-                    println!("Server is closed!");
+                    client_output
+                        .write_all("Server is closed".as_bytes())
+                        .await
+                        .unwrap();
+                    client_output.flush().await.unwrap();
                     return;
                 }
 
                 Ok(_) => {
-                    println!("{}", input);
+                    client_output.write_all(input.as_bytes()).await.unwrap();
+                    client_output.flush().await.unwrap();
+                    //println!("{}", input);
                 }
                 Err(e) => {
                     return;
